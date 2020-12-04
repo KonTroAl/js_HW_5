97e0299d7fb823d33743d2e24c643232e4769fdc
KonTroAl <troshenkin.k@yandex.ru>
2020-12-04 20:22:37 +0500

    firts task completed

--- task_1.js
+++ task_1.js
@@ -3,16 +3,32 @@
 // Строки должны нумероваться числами от 1 до 8, столбцы — латинскими буквами A, B, C, D, E, F, G, H.
 
 
-let desk = document.querySelector(".desk")
+let desk = document.querySelector(".desk");
 
-const chess_field = document.createElement("div")
-chess_field.className = "field"
+const chess_field = document.createElement("div");
+chess_field.className = "field";
 
-let filed_name = document.createElement("ul")
+let filed_name_left = document.createElement("p");
+filed_name_left.innerHTML = "8 7 6 5 4 3 2 1";
+filed_name_left.className = "field_name_left"
+
+let filed_name_right = document.createElement("p");
+filed_name_right.innerHTML = "1 2 3 4 5 6 7 8";
+filed_name_right.className = "field_name_right"
+
+let filed_name_top = document.createElement("p");
+filed_name_top.innerHTML = "HGFEDCBA";
+filed_name_top.className = "field_name_top"
+
+let filed_name_bottom = document.createElement("p");
+filed_name_bottom.innerHTML = "ABCDEFGH";
+filed_name_bottom.className = "field_name_bottom"
+
 
 function chess_desk() {
     let i = 0;
     let count = 0;
+
     while (count < 8 * 8) {
         let cell = document.createElement('div');
         chess_field.appendChild(cell);
@@ -27,6 +43,10 @@ function chess_desk() {
     }
 }
 
+desk.appendChild(filed_name_left)
+desk.appendChild(filed_name_right)
+desk.appendChild(filed_name_top)
+desk.appendChild(filed_name_bottom)
 desk.appendChild(chess_field)
 
 console.log(desk)
